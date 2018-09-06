989ce394ae23dd033e030b4254abbb0d330718c4
cb <csbutton01@hotmail.com>
2018-09-06 12:57:15 -0700

    add redux and features folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Player from './features/player';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Player />
       </div>
     );
   }
